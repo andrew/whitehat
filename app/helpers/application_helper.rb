700bb77c824cd85eea824aa9809df199054a0d52
Andrew Nesbitt <andrewnez@gmail.com>
2013-10-19 20:35:07 -0700

    Added gravatar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def gravatar_url(digest, size = '80')
+    digest ||= ''
+    "https://secure.gravatar.com/avatar/#{digest}.png?s=#{size}&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png"
+  end
 end
