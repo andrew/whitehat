95ec4dc56c37dd215892d71143f2428dce0fb9e5
Andrew Nesbitt <andrewnez@gmail.com>
2013-10-19 20:00:48 -0700

    Allow users to create issues

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,14 +1,25 @@
 class IssuesController < ApplicationController
+  before_filter :ensure_logged_in
+
   def index
-    
+    @reported_issues = current_user.issues
+  end
+  
+  def show
+    @issue = Issue.find(params[:id])
   end
   
   def new
-    
+    @issue = Issue.new
   end
   
   def create
-    
+    @issue = current_user.issues.build(params[:issue])
+    if @issue.save
+      redirect_to @issue
+    else
+      render :new
+    end
   end
   
   def edit
@@ -17,5 +28,10 @@ class IssuesController < ApplicationController
   
   def update
     
+  end
+  
+  private
+  def issue_params
+    params.require(:issue).permit(:repository, :title, :body)
   end
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,6 @@
 class Issue < ActiveRecord::Base
+  attr_accessible :repository, :title, :body
+  validates_presence_of :repository, :title, :body, :user_id
+  
   belongs_to :user
 end
