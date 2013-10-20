e6a647225106f564c83ac4b41b4c5cdcd5110bfc
Andrew Nesbitt <andrewnez@gmail.com>
2013-10-19 19:23:58 -0700

    Change login redirect path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 
     session[:user_id] = user.id
 
-    redirect_to root_path and return unless pre_login_destination
+    redirect_to issues_path and return unless pre_login_destination
     redirect_to pre_login_destination
   end
 
