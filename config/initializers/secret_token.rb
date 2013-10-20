6d61444bce74783ea8629823858808af717cac32
Andrew Nesbitt <andrewnez@gmail.com>
2013-10-19 18:09:43 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whitehat::Application.config.secret_key_base = '78a957c9426c14b4e61fb0eaf00518f7983f29e5ac9b9d90fdc9c33420f20d882f389fe892889ceb45b86acc1d26f4dab487a44e26e7492ef2ceea40bd2d58d5'
